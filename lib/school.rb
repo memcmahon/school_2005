71c7e81a01ce37bd84cf7bfe01cfc59bae98a657
memcmahon <mcmahon.meganelizabeth@gmail.com>
2020-05-08 10:07:44 -0600

    Megan's Walkthrough

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
+class School
+  attr_reader :start_time,
+              :hours_in_school_day,
+              :student_names
 
+  def initialize(start_time, hours_in_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_day
+    @student_names = []
+  end
+
+  def add_student_name(name)
+    @student_names << name
+    # @student_names.push(name)
+  end
+
+  def end_time
+    # binding.pry
+    # @start_time[0..-4].to_i
+    time = @start_time.to_i + @hours_in_school_day
+    "#{time}:00"
+  end
+
+  def is_full_time?
+    @hours_in_school_day > 4
+    # if @hours_in_school_day > 4
+    #   true
+    # else
+    #   false
+    # end
+  end
+
+  def standard_student_names
+    capitalized_names = []
+    @student_names.each do |name|
+      capitalized_names << name.capitalize
+    end
+    capitalized_names
+  end
+end
--- test/school_test.rb
+++ test/school_test.rb
@@ -1,32 +1,29 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/school'
+require 'pry'
 
 class SchoolTest < Minitest::Test
   #Iteration 1 Tests:
   def test_it_exists
-    skip
     school = School.new('9:00', 7)
 
     assert_instance_of School, school
   end
 
   def test_it_has_start_time
-    skip
     school = School.new('9:00', 7)
 
     assert_equal '9:00', school.start_time
   end
 
   def test_it_has_hours_in_school_day
-    skip
     school = School.new('9:00', 7)
 
     assert_equal 7, school.hours_in_school_day
   end
 
   def test_it_starts_with_no_student_names
-    skip
     school = School.new('9:00', 7)
 
     assert_equal [], school.student_names
@@ -34,7 +31,6 @@ class SchoolTest < Minitest::Test
 
   #Iteration 2 Tests:
   def test_it_can_add_student_names
-    skip
     school = School.new('9:00', 7)
 
     school.add_student_name('Aurora')
@@ -45,11 +41,27 @@ class SchoolTest < Minitest::Test
   end
 
   def test_it_can_calculate_end_time
-    skip
     school1 = School.new('9:00', 7)
     school2 = School.new('9:00', 3)
 
     assert_equal '16:00', school1.end_time
     assert_equal '12:00', school2.end_time
+  end
+
+  def test_it_can_determine_if_full_time
+    school1 = School.new('9:00', 7)
+    school2 = School.new('9:00', 3)
+
+    assert_equal true, school1.is_full_time?
+    assert_equal false, school2.is_full_time?
+  end
+
+  def test_it_returns_standard_student_names
+    school = School.new('9:00', 7)
+    school.add_student_name('Aurora')
+    school.add_student_name('tim')
+    school.add_student_name('megan')
+
+    assert_equal ["Aurora", "Tim", "Megan"], school.standard_student_names
   end
 end
